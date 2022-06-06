33210cfafeafcc9f9b692eefa7490b340c4a8751
imcoco <imcoco@gmail.com>
2022-06-06 19:08:24 -0400

    set up works section in app.js

--- data/works.js
+++ data/works.js
@@ -1,11 +1,12 @@
 const works = [
   {
-    image: URL ("./asset/Tic-Tac-Toe.png"),
+    image: "./asset/Tic-Tac-Toe.png",
     title: "Tic-Tac-Toe",
     description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
-    github: URL('https://github.com/imcocokim/ttt-weekend'),
-    deployment: URL('https://tictactoe-coco.netlify.app/'),
-  }
+    github: 'https://github.com/imcocokim/ttt-weekend',
+    deployment: 'https://tictactoe-coco.netlify.app/',
+  },
+
 ]
 
 export {
--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+
+/*-------------------------------- Constants --------------------------------*/
+
 import {works} from "../data/works.js"
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const workSection = document.querySelector("#work-section")
+
+/*----------------------------- Event Listeners -----------------------------*/
+console.log(works.image)
+// works.forEach(work => 
+//   console.log(work)
+//   )
+
+works.github.push("div")
+
+/*-------------------------------- Functions --------------------------------*/
