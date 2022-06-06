3fb77a1759f01b39bb42fbfee61c4767ba26da8e
imcoco <imcoco@gmail.com>
2022-06-06 13:26:58 -0400

    add data folder and works.js

---
+++ data/work.js
@@ -0,0 +1,9 @@
+const work = [
+  {
+    image: ,
+    title: "Tic-Tac-Toe",
+    description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
+    github link: URL('https://github.com/imcocokim/ttt-weekend'),
+    deployment link: URL('https://tictactoe-coco.netlify.app/'),
+  }
+]
