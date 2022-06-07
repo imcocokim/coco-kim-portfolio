68ae32b02a280eaab497e3534277c6de4b00dee3
imcoco <imcoco@gmail.com>
2022-06-07 17:15:21 -0400

    uplaod assets

--- data/works.js
+++ data/works.js
@@ -1,6 +1,6 @@
 const works = [
   {
-    image: "./asset/Tic-Tac-Toe.png",
+    image: "https://i.imgur.com/c25fpb9.png",
     title: "Tic-Tac-Toe",
     description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
     github: 'https://github.com/imcocokim/ttt-weekend',
--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@ import {works} from "../data/works.js"
 
 /*-------------------------------- Variables --------------------------------*/
 
-
+const works = {}
 
 /*------------------------ Cached Element References ------------------------*/
 
 const workSection = document.querySelector("#work-section")
 
 /*----------------------------- Event Listeners -----------------------------*/
-console.log(works.image)
-// works.forEach(work => 
-//   console.log(work)
-//   )
-
-works.github.push("div")
+// console.log(works.image)
+works.forEach(work => 
+  console.log(work.image)
+  )
+function appendWork(work, idx) {
+  let workBox = document.createElement("div")
+}
+// works.title.push("div")
+// console.log(works.title)
 
 /*-------------------------------- Functions --------------------------------*/
