cea4ce1575cbd267545cd1a37609cce2a58543b2
imcoco <imcoco@gmail.com>
2022-06-07 18:52:43 -0400

    change html nav into ul/li

--- data/works.js
+++ data/works.js
@@ -7,6 +7,28 @@ const works = [
     deployment: 'https://tictactoe-coco.netlify.app/',
   },
 
+  {
+    image: '/assets/images/ttt.png',
+    title: "Tic-Tac-Toe",
+    description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
+    github: 'https://github.com/imcocokim/ttt-weekend',
+    deployment: 'https://tictactoe-coco.netlify.app/',
+  },
+  {
+    image: '/assets/images/ttt.png',
+    title: "Tic-Tac-Toe",
+    description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
+    github: 'https://github.com/imcocokim/ttt-weekend',
+    deployment: 'https://tictactoe-coco.netlify.app/',
+  },
+  {
+    image: '/assets/images/ttt.png',
+    title: "Tic-Tac-Toe",
+    description: "Tic-Tac-Toe is one of the first games that we had to creat at General Assembly.",
+    github: 'https://github.com/imcocokim/ttt-weekend',
+    deployment: 'https://tictactoe-coco.netlify.app/',
+  },
+
 ]
 
 export {
