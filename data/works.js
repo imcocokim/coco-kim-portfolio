8d3bb18c92b15431009fd661743d1110105d7206
imcoco <imcoco@gmail.com>
2022-07-01 08:42:26 -0400

    Add Skeleton Kitchen

--- data/works.js
+++ data/works.js
@@ -8,11 +8,11 @@ const works = [
   },
 
   {
-    image: '/assets/images/placeholder-1.png',
-    title: "Placeholder",
-    description: "Click the links to see more food.",
-    github: 'https://www.behance.net/imcoco',
-    deployment: 'https://dayinthekitchen.wordpress.com/'
+    image: '/assets/images/sitk.png',
+    title: "Skeletons in the Kitchen",
+    description: "Where cooks can go and be honest about their work environments.",
+    github: 'https://github.com/imcocokim/skeleton-kitchen',
+    deployment: 'https://skeleton-kitchen.herokuapp.com/'
   },
   {
     image: '/assets/images/placeholder-2.png',
