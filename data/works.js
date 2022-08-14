f567582544267eb6d345e09a029804ed9f6680d9
imcoco <imcoco@gmail.com>
2022-08-14 16:30:45 -0400

    update resume

--- data/works.js
+++ data/works.js
@@ -2,7 +2,7 @@ const works = [
   {
     image: '/assets/images/ttt.png',
     title: "Tic-Tac-Toe",
-    description: "A class game of tic-tac-toe.",
+    description: "A class game of tic-tac-toe built with JavaScript, HTML, and CSS.",
     github: 'https://github.com/imcocokim/ttt-weekend',
     deployment: 'https://tictactoe-coco.netlify.app/'
   },
@@ -10,21 +10,21 @@ const works = [
   {
     image: '/assets/images/sitk.png',
     title: "Skeletons in the Kitchen",
-    description: "Where cooks can go and be honest about their work environments.",
+    description: "A MERN-stack, full CRUD, Google OAuth, Node, Express, and MongoDB application that lets cooks rate and review their working environment with the culinary industry.",
     github: 'https://github.com/imcocokim/skeleton-kitchen',
     deployment: 'https://skeleton-kitchen.herokuapp.com/'
   },
   {
     image: '/assets/images/CHEERS.png',
     title: "CHEERS",
-    description: "Where users can pair their favorite songs with their favorite drinks",
+    description: "A decoupled MERN-stack and full CRUD application with API implementation that lets users pair their favorite song with favorite drinks",
     github: 'https://github.com/Recipra/cheers-front-end',
     deployment: 'https://cheers-ga.netlify.app/'
   },
   {
     image: '/assets/images/potato.png',
     title: "POTATO",
-    description: "A place where non-profit organizations can be added with their events.",
+    description: "A full-stack app using Python-based Django Web Framework where users collect non-profit organizations and their events.",
     github: 'https://github.com/imcocokim/npo-collector',
     deployment: 'https://npo-collector.herokuapp.com/'
   }
